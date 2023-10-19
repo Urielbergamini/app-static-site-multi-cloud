0d241a9e7debd5768897fafaffcaf074187641ac
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-18 21:26:47 -0300

    sjasddjsa

--- terraform/azure/provider.tf
+++ terraform/azure/provider.tf
@@ -2,14 +2,12 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
+      version = "~>3.0"
       version = ">= 3.76.0"
     }
   }
+}
 
-backend "azurerm" {
-    resource_group_name  = "cloud-shell-storage-eastus"
-    storage_account_name = "cs21003200260fb77bb"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
+provider "azurerm" {
+  features {}
 }
